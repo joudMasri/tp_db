6b005afca28d99a269ed594b057eda156f9422ce
Joud Masri <joudmasri679@gmail.com>
2022-11-28 10:25:42 +0200

    first commit

---
+++ UI+SQLServer/Form2.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace UI_SQLServer
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_command = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_add_description = new System.Windows.Forms.TextBox();
+            this.txt_add_genre = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_add_language = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txt_delete_title = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.lb_rows_affected = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_add_title = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.excute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(607, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Command :";
+            // 
+            // txt_command
+            // 
+            this.txt_command.Location = new System.Drawing.Point(298, 67);
+            this.txt_command.Name = "txt_command";
+            this.txt_command.Size = new System.Drawing.Size(763, 47);
+            this.txt_command.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-19, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(2567, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "---------------------------------------------------------------------------------" +
+    "---------------------------------------------------------------------";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 156);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 39);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "New Video :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-1, 195);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(221, 39);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "------------";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(15, 282);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 31);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Description :";
+            // 
+            // txt_add_description
+            // 
+            this.txt_add_description.Location = new System.Drawing.Point(177, 272);
+            this.txt_add_description.Name = "txt_add_description";
+            this.txt_add_description.Size = new System.Drawing.Size(281, 47);
+            this.txt_add_description.TabIndex = 6;
+            // 
+            // txt_add_genre
+            // 
+            this.txt_add_genre.Location = new System.Drawing.Point(607, 272);
+            this.txt_add_genre.Name = "txt_add_genre";
+            this.txt_add_genre.Size = new System.Drawing.Size(281, 47);
+            this.txt_add_genre.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(502, 282);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 31);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Genre :";
+            // 
+            // txt_add_language
+            // 
+            this.txt_add_language.Location = new System.Drawing.Point(1064, 272);
+            this.txt_add_language.Name = "txt_add_language";
+            this.txt_add_language.Size = new System.Drawing.Size(281, 47);
+            this.txt_add_language.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(932, 282);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 31);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Language :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(-594, 414);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(2567, 39);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "---------------------------------------------------------------------------------" +
+    "---------------------------------------------------------------------";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(-1, 531);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(238, 39);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "-------------";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(15, 492);
+            this.label10.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(216, 39);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Delete Video :";
+            // 
+            // txt_delete_title
+            // 
+            this.txt_delete_title.Location = new System.Drawing.Point(107, 587);
+            this.txt_delete_title.Name = "txt_delete_title";
+            this.txt_delete_title.Size = new System.Drawing.Size(281, 47);
+            this.txt_delete_title.TabIndex = 15;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(15, 597);
+            this.label11.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(83, 31);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Title :";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(-607, 716);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(2567, 39);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "---------------------------------------------------------------------------------" +
+    "---------------------------------------------------------------------";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(-1, 794);
+            this.label13.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(425, 39);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "------------------------";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(15, 755);
+            this.label14.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(401, 39);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "NBR Of Records Affected :";
+            // 
+            // lb_rows_affected
+            // 
+            this.lb_rows_affected.AutoSize = true;
+            this.lb_rows_affected.Location = new System.Drawing.Point(414, 755);
+            this.lb_rows_affected.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lb_rows_affected.Name = "lb_rows_affected";
+            this.lb_rows_affected.Size = new System.Drawing.Size(34, 39);
+            this.lb_rows_affected.TabIndex = 19;
+            this.lb_rows_affected.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1212, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 55);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1212, 672);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 55);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_add_title
+            // 
+            this.txt_add_title.Location = new System.Drawing.Point(607, 159);
+            this.txt_add_title.Name = "txt_add_title";
+            this.txt_add_title.Size = new System.Drawing.Size(281, 47);
+            this.txt_add_title.TabIndex = 23;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label15.Location = new System.Drawing.Point(502, 169);
+            this.label15.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(83, 31);
+            this.label15.TabIndex = 22;
+            this.label15.Text = "Title :";
+            // 
+            // excute
+            // 
+            this.excute.Location = new System.Drawing.Point(1212, 67);
+            this.excute.Name = "excute";
+            this.excute.Size = new System.Drawing.Size(142, 55);
+            this.excute.TabIndex = 24;
+            this.excute.Text = "EXCUTE";
+            this.excute.UseVisualStyleBackColor = true;
+            this.excute.Click += new System.EventHandler(this.excute_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 39F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1366, 841);
+            this.Controls.Add(this.excute);
+            this.Controls.Add(this.txt_add_title);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lb_rows_affected);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txt_delete_title);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_add_language);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_add_genre);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_add_description);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_command);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txt_command;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txt_add_description;
+        private TextBox txt_add_genre;
+        private Label label6;
+        private TextBox txt_add_language;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox txt_delete_title;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label lb_rows_affected;
+        private Button button1;
+        private Button button2;
+        private TextBox txt_add_title;
+        private Label label15;
+        private Button excute;
+    }
+}
---
+++ UI+SQLServer/Form2.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace UI_SQLServer
+{
+    public partial class Form2 : Form
+    {
+        int rows = 0;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        Boolean SqlComExec(string s)
+        {
+            if (s.Length > 0)
+            {
+                try
+                {
+                    string _connectionString = @"Data Source=JOUD\INSTANCE_2K19_01;Initial Catalog=mydb;Integrated Security=True";
+
+                    using (SqlConnection _con = new SqlConnection(_connectionString))
+                    {
+                        _con.Open();
+                        string queryStatement = s;
+                        using (SqlCommand _cmd = new SqlCommand(queryStatement, _con))
+                        {
+                            _cmd.ExecuteNonQuery();
+                            rows++;
+                        }
+                    }
+                    MessageBox.Show("Done");
+                    return true;
+                }
+                catch
+                {
+                    MessageBox.Show("Enter a Correct Statement Please");
+                }
+            }
+            return false;
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string _connectionString = @"Data Source=JOUD\INSTANCE_2K19_01;Initial Catalog=mydb;Integrated Security=True";
+
+            using (SqlConnection _con = new SqlConnection(_connectionString))
+            {
+                _con.Open();
+                string queryStatement = "INSERT INTO mydb.dbo.video(title,video_desc,genre,langue)VALUES('" + txt_add_title.Text.ToString() + "','" + txt_add_description.Text.ToString() + "', '" + txt_add_genre.Text.ToString() + "','" + txt_add_language.Text.ToString() + "')";
+                
+                using (SqlCommand _cmd  = new SqlCommand(queryStatement, _con))
+                {
+                    _cmd.ExecuteNonQuery();
+                    rows++;
+                }
+            }
+            txt_add_title.Text = "";
+            txt_add_description.Text = "";
+            txt_add_genre.Text = "";
+            txt_add_language.Text = "";
+            lb_rows_affected.Text = rows.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string _connectionString = @"Data Source=JOUD\INSTANCE_2K19_01;Initial Catalog=mydb;Integrated Security=True";
+            using (SqlConnection _con = new SqlConnection(_connectionString))
+            {
+                _con.Open();
+                string queryStatement = "DELETE FROM mydb.dbo.video WHERE(title ='" + txt_delete_title.Text.ToString() + "')";
+                
+                using (SqlCommand _cmd = new SqlCommand(queryStatement, _con))
+                {
+                    _cmd.ExecuteNonQuery();
+                    rows++;
+                }
+            }
+            txt_delete_title.Text = "";
+            lb_rows_affected.Text = rows.ToString();
+        }
+
+        private void excute_Click(object sender, EventArgs e)
+        {
+
+            SqlComExec(txt_command.Text);
+            txt_command.Text = "";
+        }
+    }
+}
